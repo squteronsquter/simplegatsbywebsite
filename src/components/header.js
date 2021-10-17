69250266bbd75b590de44fa7fd2d4189c55b9a53
squteronsquter <squter@squter.pl>
2021-10-17 03:49:38 +0200

    styled index and about pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 export default function header(props) {
     return (
-        <div>
-            <h1>{props.headerText}</h1>
+        <div className="wrapper">
+            <Link to="/" className="link">Home</Link>
+            <Link to="/about" className="link">About</Link>
+            <h1 className={props.styleClasses}>{props.headerText}</h1>
         </div>
     )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import Header from '../components/header'
+import * as styles from './about.module.css'
 import Container from '../components/container'
+
+const User = props => (
+    <div className={styles.user}>
+        <h2 className={styles.username}>{props.username}</h2>
+        <img src={props.avatar} className={styles.avatar} alt={props.title}/>
+        <caption className={styles.description}>{props.description}</caption>
+    </div>
+)
+
 
 export default () => (
     <Container>
-        <Header headerText="About"/>
+        {/* className through props from Header first and then global css */}
+        <Header headerText="About Us Page" styleClasses="centered"/>
+        {/* className from global.css */}
+        <p className="centered">CSS modules were used to style this page'c container.</p>
+        <User 
+        username="Giullietta"
+        avatar="/avalon-avatar.jpg"
+        description="Photo by Jeremy Killpatrick" />
+        <User 
+        username="Margot"
+        avatar="/avalon-avatar-03.jpg"
+        description="Photo by Jeremy Killpatrick" />
+        <User 
+        username="Julia"
+        avatar="/avalon-avatar-02.jpg"
+        description="Photo by Jeremy Killpatrick" />
     </Container>
+
 )
